c6718562743a40290c1c8e9a06b5026bb5d88d5c
mike Tetteh <miketetteh751@outlook.com>
2020-01-09 18:45:08 +0000

    :(

--- store/me_try/models.py
+++ store/me_try/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+# import mptt
+# from django.contrib.auth.models import Group
+# from mptt.fields import TreeForeignKey
 
 class Genre(MPTTModel):
     name = models.CharField(max_length=50, unique=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
-
+    # add a parent foreign key
+    # parent = TreeForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True).contribute_to_class(Group, 'parent')
+    # mptt.register(Group,order_insertion_by=['name'])
+    
     class MPTTMeta:
         order_insertion_by = ['name']
+
+
+
+
+
