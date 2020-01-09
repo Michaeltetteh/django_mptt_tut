193c6b05299ec63b52196994551e3a660d20338c
mike Tetteh <miketetteh751@outlook.com>
2020-01-09 17:59:05 +0000

    get_descendants

--- store/me_try/urls.py
+++ store/me_try/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import show_genres
+from .views import show_genres,show_des
 
 
 urlpatterns = [
-    path('genres/', show_genres),
+    path('genres/', show_des),
 ]
--- store/me_try/views.py
+++ store/me_try/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from .models import Genre
+from itertools import chain
+from typing import Iterable
 
+def get_descendants(genre: Genre) -> Iterable[Genre]:
+    qs = Genre.objects.filter(parent=genre)
+    rs = chain(qs)
+    for child in qs:
+        rs = chain(rs, get_descendants(child))
+    return rs
+
+
+def show_des(request):
+    return render(request,"genres.html",{'des':list(get_descendants(Genre.objects.get(pk=1)))})
 
 def show_genres(request):
     return render(request, "genres.html", {'genres': Genre.objects.all()})
